87fe8c1d8fb02f0cf530ad36c2c4af7617c4e947
menonf <menon.f@hotmail.com>
2019-06-08 16:36:01 +0100

    Fix1

--- PortfolioTheory/Markovitz.py
+++ PortfolioTheory/Markovitz.py
@@ -1,17 +1,19 @@
-import quandl
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tiingo import TiingoClient
 
-# get adjusted closing prices of 5 selected companies with Quandl
-quandl.ApiConfig.api_key = 'puxYR3syR5S-DyzWszgu'
-stocks = ['AAPL', 'AMZN', 'GOOGL', 'FB']
-data = quandl.get_table('WIKI/PRICES', ticker=stocks, qopts={'columns': ['date', 'ticker', 'adj_close']},
-                        date={'gte': '2016-1-1', 'lte': '2018-07-26'}, paginate=True)
+# Tiingo API call to fetch data
+config = {}
+config['session'] = True
+config['api_key'] = "81f0783ae3d1756869af76d72b52a86f08e2ca15"
 
-new = data.set_index('date')
-table = new.pivot(columns='ticker')
-returns = table.pct_change()
+client = TiingoClient(config)
+stocks = ['MSFT','AAPL', 'AMZN', 'GOOGL', 'FB']
+ticker_history = client.get_dataframe(stocks, frequency='daily',
+                                      metric_name='adjClose', startDate='2017-05-05', endDate='2019-05-18')
+# calculate daily returns
+returns = ticker_history.pct_change()
 
 # calculate mean daily return and covariance of daily returns
 mean_daily_returns = returns.mean()
@@ -20,15 +22,14 @@ cov_matrix = returns.cov()
 # set number of runs of random portfolio weights
 num_portfolios = 25000
 
-# set up array to hold results
-# We have increased the size of the array to hold the weight values for each stock
+# set up an array to hold results
+# I have increased the size of the array to hold the weight values for each stock
 results = np.zeros((4 + len(stocks) - 1, num_portfolios))
 
 for i in range(num_portfolios):
     # select random weights for portfolio holdings
-    weights = np.array(np.random.random(4))
-    # rebalance weights to sum to 1
-    weights /= np.sum(weights)
+    weights = np.array(np.random.random(len(stocks)))
+    weights = weights / np.sum(weights)
 
     # calculate portfolio return and volatility
     portfolio_return = np.sum(mean_daily_returns * weights) * 252
@@ -37,14 +38,17 @@ for i in range(num_portfolios):
     # store results in results array
     results[0, i] = portfolio_return
     results[1, i] = portfolio_std_dev
+
     # store Sharpe Ratio (return / volatility) - risk free rate element excluded for simplicity
     results[2, i] = results[0, i] / results[1, i]
+
     # iterate through the weight vector and add data to results array
     for j in range(len(weights)):
         results[j + 3, i] = weights[j]
 
 # convert results array to Pandas DataFrame
-results_frame = pd.DataFrame(results.T, columns=['Returns', 'stdev', 'sharpe', stocks[0], stocks[1], stocks[2], stocks[3]])
+col1 = ['Returns', 'stdev', 'sharpe'] + stocks
+results_frame = pd.DataFrame(results.T, columns=col1)
 
 # locate position of portfolio with highest Sharpe Ratio
 max_sharpe_port = results_frame.iloc[results_frame['sharpe'].idxmax()]
@@ -56,6 +60,7 @@ plt.scatter(results_frame.stdev, results_frame.Returns, c=results_frame.sharpe, 
 plt.xlabel('Volatility')
 plt.ylabel('Returns')
 plt.colorbar()
+
 # plot red star to highlight position of portfolio with
 plt.scatter(max_sharpe_port[1], max_sharpe_port[0], marker= '*', color='r', label="Max Sharpe Ratio", s=200)
 # plot green star to highlight position of minimum variance portfolio
